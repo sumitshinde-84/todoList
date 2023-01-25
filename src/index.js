03a64740eba9f6b37d0ae0c4335f5ed97c917523
sumitshinde-84 <shindesumit217@gmail.com>
2023-01-25 21:03:22 +0530

    add sidebar animation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import "./styles/main.css";
+import "./styles/varriables.css";
 
-console.log("pop");
-
-console.log("ykeepi");
+const menuIcon = document.querySelector("#menuIcon");
+const main = document.querySelector(".main");
+menuIcon.addEventListener("click", (event) => {
+ 
+  main.style.gridTemplateColumns = "0 100%";
+  event.target.addEventListener("click", () => {
+    main.style.gridTemplateColumns = "350px 1400px";
+  });
+});
