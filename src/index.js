01bda7495d987340e2b3b891dd771389badb570d
sumitshinde-84 <shindesumit217@gmail.com>
2023-01-26 17:09:47 +0530

    add list animation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 import "./styles/main.css";
 import "./styles/varriables.css";
+import "./styles/header.css";
+import "./styles/sidebar.css";
+import "./styles/mainContent.css";
+import "./styles/footer.css";
+import {
+  menuIcon, mainContent, sideBar, list,
+} from "./modules/domCollection";
 
-const list = document.querySelector("li");
-list.addEventListener("click", () => {
-  list.style.borderLeft = "3px solid var( --accentDark)";
-  list.style.borderRadius = "3px";
-  list.style.backgroundColor = "#2abd6750";
-});
+
+
+// ------------------------------toggle menu animation----------------------------------------------------
+
+(function () {
+  let iconClick = "false";
+  menuIcon.addEventListener("click", () => {
+    if (iconClick === "false") {
+      mainContent.style.gridColumn = "1/3";
+      sideBar.style.gridColumn = "";
+      sideBar.style.transform = "translate(-110%,0%)";
+      iconClick = "true";
+    } else if (iconClick === "true") {
+      mainContent.style.gridColumn = "2/3";
+      sideBar.style.gridColumn = "1/2";
+      sideBar.style.transform = "translate(0%,0%)";
+      iconClick = "false";
+    }
+  });
+}());
+
+// ------------------------------list click Animation----------------------------------------------------
+
+(function () {
+  for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", () => {
+      for (let j = 0; j < list.length; j++) {
+        list[j].style.borderLeft = "none";
+        list[j].style.borderRadius = "";
+        list[j].style.backgroundColor = "";
+      }
+      list[i].style.borderLeft = "2px var(--accentDark) solid";
+      list[i].style.borderRadius = "2px";
+      list[i].style.backgroundColor = "#2abd675f";
+    });
+  }
+}());
