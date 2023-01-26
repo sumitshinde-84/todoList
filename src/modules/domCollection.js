ae316ca1f17f140d43ba783f6f19681013c5a607
sumitshinde-84 <shindesumit217@gmail.com>
2023-01-26 17:20:48 +0530

    add list animaiton

---
+++ src/modules/domCollection.js
@@ -0,0 +1,4 @@
+export const menuIcon = document.querySelector("img");
+export const sideBar = document.querySelector(".sidebar");
+export const mainContent = document.querySelector(".mainContent");
+export const list = document.querySelectorAll("li");
